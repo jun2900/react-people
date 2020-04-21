860f871e01660dbe302572a7d83b0175a10196d2
jun2900 <jeremiahmaramis00@gmail.com>
2020-04-21 15:51:41 +0800

    commit1

--- src/App.js
+++ src/App.js
@@ -32,26 +32,50 @@ import "./App.css";
 //   </>
 // }
 
+
+
+// const List = props => {
+//   return (
+//     <>
+//       <h1>React People!</h1>
+//       <ul className="list">
+//         {props.data.sort(sortByPower).map((person) => (
+//           <li key={person.name} className="list-item">
+//             <div className="list-item-image-container">
+//               <img src={person.thumbnail} alt="telur" />
+//             </div>
+//             <span className="list-item-name">
+//               {person.name} (Power:{' '}
+//               <span className="list-item-power">{person.power}</span>)
+//             </span>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   )
+// }
+
 const sortByPower = (a, b) => {
   return b.power - a.power;
 }
 
-const List = props => {
+const List = (props) => {
   return (
     <>
-      <h1>React People!</h1>
-      <ul className="list">
-        {props.data.sort(sortByPower).map((person) => (
-          <li key={person.name} className="list-item">
-            <div className="list-item-image-container">
-              <img src={person.thumbnail} alt="telur" />
-            </div>
-            <span className="list-item-name">
-              {person.name} (Power:{' '}
-              <span className="list-item-power">{person.power}</span>)
-            </span>
-          </li>
-        ))}
+      <ul className='list'>
+        {
+          props.data.sort(sortByPower).map(user => {
+            return <li key={user.name} className='list-item'>
+              <div className="list-item-image-container">
+                {/* <img src={`https://robohash.org/${user.id}?set=set1`} /> */}
+              </div>
+              <span className="list-item-name">
+                {user.name} ( Power:{` `}
+                <span className="list-item-power">{user.email}</span>)
+              </span>
+            </li>
+          })
+        }
       </ul>
     </>
   )
@@ -78,14 +102,24 @@ class App extends React.Component {
           thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
           power: 30
         }
-      ]
+      ],
+      users: [],
+      searchString: ``
     }
   }
+
+  async componentDidMount() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const json = await response.json()
+    this.setState({ users: json })
+    console.log(this.state.users[0]);
+  }
+
   render() {
     return (
       <>
         <div className="App">
-          <List data={this.state.human} />
+          <List data={this.state.users} />
         </div>
       </>
     );
